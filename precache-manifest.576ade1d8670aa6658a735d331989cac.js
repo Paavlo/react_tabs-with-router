ecb1dc324b0ee55844777ff6695f72b0dac222f6
Paavlo <pavlo.com.5@gmail.com>
2020-09-23 21:16:27 +0300

    Updates

---
+++ precache-manifest.576ade1d8670aa6658a735d331989cac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0dbd5314808a8ebb99e2bdd3fa818f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ee7cd49f316936ab95f8",
+    "url": "./static/css/main.d07a364d.chunk.css"
+  },
+  {
+    "revision": "528422c7c0ddad648bdc",
+    "url": "./static/js/2.6a701b62.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.6a701b62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee7cd49f316936ab95f8",
+    "url": "./static/js/main.2bfedee3.chunk.js"
+  },
+  {
+    "revision": "12bee12ab4b288faf1d5",
+    "url": "./static/js/runtime-main.108729f7.js"
+  }
+]);
